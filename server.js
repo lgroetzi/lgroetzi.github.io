06c6bf6c614218344e211342099d0fc9ddb75ca8
Lincoln de Sousa <lincoln@clarete.li>
2020-07-13 21:52:40 -0400

    Fix annoying redirect issue

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+const PAGES_WE_HANDLE = [
+  '/',
+  '/collabs',
+  '/justworks',
+];
+
+app.prepare().then(() => {
+  const server = express()
+
+  PAGES_WE_HANDLE.forEach(page => server.get('/collabs', (req, res) =>
+    app.render(req, res, '/collabs', req.query)
+  ));
+
+  server.all('*', (req, res) => handle(req, res));
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
