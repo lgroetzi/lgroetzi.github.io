392bcf4cb04a0fcea7069597912a7c1d97bc606c
Lincoln de Sousa <lincoln@clarete.li>
2020-07-19 19:31:32 -0400

    Update static website

---
+++ _next/static/9OshiKM9DUH3oB74SqHwr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/collabs":[a],"/index":[a],"/justworks":[a],"/selfmade1.0":[a],"/selfmade2.0":[a]}}("static\u002Fchunks\u002F47fdffac67954badd230e1efd99dd4c65b29a808.17d2feffbbecec686ca6.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
