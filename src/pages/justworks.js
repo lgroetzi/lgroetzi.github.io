0eb9094b02279bf3a2ef852e846eb0197b74df76
Lincoln Clarete <lincoln@clarete.li>
2020-08-02 14:18:37 -0400

    Justworks: Mobile version & refactoring on desktop to use components

--- src/pages/justworks.js
+++ src/pages/justworks.js
@@ -2,12 +2,16 @@ import * as React from "react";
 import Link from 'next/link';
 
 import { LightMenu } from '../components/menus';
-import { GlamourShot, Feat } from '../components/project'
+import { GlamourShot, Feat, Details, Next } from '../components/project';
+
+import { getResponsiveness, responsiveClasses } from '../lib/media-query';
 
 export default function Justworks() {
+  const r = getResponsiveness();
+  const className = responsiveClasses(r);
   return (
     <div className="project-page">
-      <LightMenu />
+      {!r.isMobile && <LightMenu />}
 
       <GlamourShot
         title="Justworks"
@@ -15,22 +19,21 @@ export default function Justworks() {
         img="/img/JW-1@2x.png"
       />
 
-      <Feat img="/img/JW-2@2x.png" />
+      <Feat
+        color={r.isMobile ? "blue" : ""}
+        img={`/img/${r.isMobile ? "Justworks-1_Mobile.png" : "JW-2@2x.png"}` } />
 
-      <div
-        className="project-details"
-        style={{ padding: "260px 90px 0 90px" }}
-      >
-        <div>
-          <img
-            className="box-shadow"
-            width="387px"
-            src="/img/JW-3@2x.png" />
-        </div>
+      <Details>
+        {!r.isMobile &&
+         <div>
+           <img
+             className="box-shadow"
+             width="387px"
+             src="/img/JW-3@2x.png" />
+         </div>}
 
         <div>
           <h1>The Business</h1>
-
           <p>
             Justworks provides simple software for payroll, benefits, HR, and compliance.
           </p>
@@ -40,70 +43,66 @@ export default function Justworks() {
             Given higher churn with small companies, we needed to onboard them at a low cost. Justworks’ onboarding involves complex forms (e.g. employee censuses). Our challenge was to build a fully automated onboarding flow that would make onboarding small companies worthwhile.
           </p>
         </div>
-      </div>
+      </Details>
 
-      <div
-        className="project-feat"
-        style={{
-          marginTop: "-36px",
+      { r.isMobile
+        ? <img src="/img/Justworks-2_Mobile.png" style={{ maxWidth: "calc(100% + 120px)" }} />
+        : <Feat img="/img/JW-4@2x.png" style={{
+          marginTop: "-94px",
           paddingTop: "126px",
-        }}
-      >
-        <img
-          className="box-shadow"
-          src="/img/JW-4@2x.png"
-        />
-      </div>
+        }} /> }
 
-      <div
-        className="project-details no-bg"
-        style={{ paddingBottom: "90px" }}
-      >
+      <Details className="no-bg">
+        {!r.isMobile &&
+         <div>
+           <img
+             className="box-shadow"
+             width="572px"
+             src="/img/JW-5@2x.png" />
+         </div>}
+
         <div>
-          <img
-            className="box-shadow"
-            width="572px"
-            src="/img/JW-5@2x.png" />
-        </div>
-
-        <div style={{ marginRight: 0 }}>
           <h1>The Product</h1>
           <p>
             After redesigning the marketing page, we automated payroll setup and created online applications for healthcare quotes and workers’ comp.
           </p>
-
           <h1>The Outcome</h1>
           <p>
-          With the launch of our redesigned pricing page, we tripled the number of signups. With our streamlined, self-serve onboarding flow, we increased the setup completion rate of new users from 23% to 44%.
+            With the launch of our redesigned pricing page, we tripled the number of signups. With our streamlined, self-serve onboarding flow, we increased the setup completion rate of new users from 23% to 44%.
           </p>
         </div>
-      </div>
-      
-      <div>
-        <img
-          className="box-shadow"
-          width="100%"
-          src="/img/JW-6@2x.png" />
-      </div>
+      </Details>
 
-      <div
-        style={{
-          background: "#f2f2f2",
-          padding: "90px",
-          textAlign: "center",
-        }}
-      >
-        <img
-          className="box-shadow"
-          width="1100px"
-          src="/img/JW-7@2x.png"
-        />
-      </div>
+      {!r.isMobile &&
+       <div>
+         <div>
+           <img
+             className="box-shadow"
+             width="100%"
+             src="/img/JW-6@2x.png" />
+         </div>
+         <div
+           style={{
+             background: "#f2f2f2",
+             padding: "90px",
+             textAlign: "center",
+           }}
+         >
+           <img
+             className="box-shadow"
+             width="1100px"
+             src="/img/JW-7@2x.png"
+           />
+         </div>
+       </div>}
 
-      <div className="project-next">
-        <Link href="/collabs"><a>View Next Project</a></Link>
-      </div>
+      {r.isMobile &&
+       <div>
+         <Feat img="/img/Justworks-3_Mobile.png" />
+         <Feat img="/img/Justworks-4_Mobile.png" color="blue" />
+       </div>}
 
+      <Next link="/collabs" />
     </div>
   );
 }
