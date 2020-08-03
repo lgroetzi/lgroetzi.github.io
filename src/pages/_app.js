130b78f8342f764b506ac178923d03cbb5e6def6
Lincoln Clarete <lincoln@clarete.li>
2020-08-02 23:04:03 -0400

    Awful way to get media queries to work by throwing ssr away

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,34 @@
+import React from 'react';
+import NextApp from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { Context as ResponsiveContext } from 'react-responsive'
+
 import "../global.css"
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = {}
+
+export default class App extends NextApp {
+  componentDidMount() {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode)
+      jssStyles.parentNode.removeChild(jssStyles);
+    this.setState({ width: window.innerWidth });
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    try {
+      window
+    } catch (e) {
+      return null;
+    }
+
+    return (
+      <ResponsiveContext.Provider>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ResponsiveContext.Provider>
+    );
+  }
 }
