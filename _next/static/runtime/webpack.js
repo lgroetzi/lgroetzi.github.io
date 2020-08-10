f59b529060001eb8ffd03c00d787e694acd2ba5d
Lincoln de Sousa <lincoln@comum.org>
2020-08-09 22:54:11 -0400

    Update static website

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f87b19825f1114069c09";
+/******/ 	var hotCurrentHash = "81505c1ede3f92c11bbb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
