b5622a17f9d654d32a9289265b47e21f8797b70c
Lincoln de Sousa <lincoln@comum.org>
2020-08-09 23:01:00 -0400

    Update static website

---
+++ _next/static/OIo0iLkh5W8lOt2Kxzuut/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/collabs":[a],"/index":[a],"/justworks":[a],"/selfmade1.0":[a],"/selfmade2.0":[a]}}("static\u002Fchunks\u002F6d11b6451d6a80093020dacc9bf37409478d49d1.007f03f3f96a47ea4238.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1a3381be54dadfd1c9cd";
+/******/ 	var hotCurrentHash = "58e6278b9460697b2528";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
