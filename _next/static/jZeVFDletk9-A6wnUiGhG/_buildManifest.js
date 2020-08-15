5ca1be3105f4b2c14aa1e46e6f5867bb35c6d37f
Lincoln de Sousa <lincoln@comum.org>
2020-08-15 16:35:53 -0400

    Update static website

---
+++ _next/static/jZeVFDletk9-A6wnUiGhG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/collabs":[a],"/index":[a],"/justworks":[a],"/selfmade1.0":[a],"/selfmade2.0":[a]}}("static\u002Fchunks\u002Ff7b3a86be1f8415b564a8856070828d5e939cc75.a84e5b3f4c2b26e03d18.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
